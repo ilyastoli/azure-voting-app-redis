942184014e61b31affe4d2e971090f7992854109
ilyastoli <52413756+ilyastoli@users.noreply.github.com>
2019-08-03 12:21:44 +0300

    Update config.sh

--- kube/config.sh
+++ kube/config.sh
@@ -7,7 +7,7 @@ do
   key=$(echo "$line" | awk -F":" '{print $1}')
   value=$(echo "$line" | awk -F":" '{print $2}')
   echo "key=${key}, val=${value}"
+  sed 's/${key}/${value}/g' ${WORKDIR}/azure-vote-all-in-one-redis.yaml
   sed -i 's/${key}/${value}/g' ${WORKDIR}/azure-vote-all-in-one-redis.yaml
 done < "$INPUT"
 
-cat ${WORKDIR}/azure-vote-all-in-one-redis.yaml
