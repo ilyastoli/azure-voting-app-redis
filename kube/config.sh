c36e2ca0f395961fa9aa5b5becf461931068fd60
ilyastoli <52413756+ilyastoli@users.noreply.github.com>
2019-08-03 11:59:36 +0300

    Update config.sh

--- kube/config.sh
+++ kube/config.sh
@@ -3,5 +3,5 @@ while IFS= read -r line
 do
   key=$(awk -F":" '{print $1}' params)
   value=$(awk -F":" '{print $2}' params)
-  ehco "key=${key}, val=${value})
+  ehco "key=${key}, val=${value}"
 done < "$input"
