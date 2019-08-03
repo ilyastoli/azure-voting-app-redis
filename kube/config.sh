8775a2cef992e58dd5dfa3ff82e3410dd439ea99
ilyastoli <52413756+ilyastoli@users.noreply.github.com>
2019-08-03 12:11:39 +0300

    Update config.sh

--- kube/config.sh
+++ kube/config.sh
@@ -2,7 +2,7 @@ input="$WORKSPACE/kube/params"
 pwd;
 while IFS= read -r line
 do
-  key=$(echo "$line" | awk -F":" '{print $1})
-  alue=$(echo "$line" | awk -F":" '{print $2})
+  key=$(echo "$line" | awk -F":" '{print $1}')
+  alue=$(echo "$line" | awk -F":" '{print $2}')
   echo "key=${key}, val=${value}"
 done < "$input"
